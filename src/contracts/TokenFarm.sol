a2721cd7c754a4e6970647718e1d99a0833bf7aa
Vishal Sutariya <SVishuS@1998>
2022-04-08 15:18:40 +0530

    deployment 2

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,5 +1,16 @@
 pragma solidity ^0.5.0;
 
+import "./DaiToken.sol";
+import "./DappToken.sol"; 
+
 contract TokenFarm{
     string public name = "DApp Token Farm";
+    DappToken public dappToken;
+    DaiToken public daiToken;
+
+    constructor(DappToken _dappToken, DaiToken _daiToken) public {
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+
+    }
 }
