3b6e93e819395d3b638acf99c074c5ef4ef8fba2
Vishal Sutariya <SVishuS@1998>
2022-04-09 11:03:13 +0530

    test updated

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -1,16 +1,24 @@
-const { assert } = require('chai')
-
 const DaiToken = artifacts.require('DaiToken')
 const DappToken = artifacts.require('DappToken')
 const TokenFarm = artifacts.require('TokenFarm')
 
 require('chai').use(require('chai-as-promised')).should()
 
+function tokens(n){
+    return web3.utils.toWei(n, 'Ether')
+}
+
 contract('TokenFarm', (accounts) => {
 
-    let daiToken;
+    let daiToken, dappToken, tokenFarm;
     before(async () => {
+        //load contracts
         daiToken = await DaiToken.new()
+        dappToken = await DappToken.new()
+        tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address)
+
+        // transfer all dapp token to farm (1 million)
+        await dappToken.transfer(tokenFarm.address,  tokens('1000000'))
     })
 
     //tests
